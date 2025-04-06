434e4efc01263e37390774a9617b4f32da6d5a9c
makarova.a63 <makarova.a63@wb.ru>
2025-04-06 10:52:44 +0300

    Initial Commit

---
+++ Roamly/RoamlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoamlyApp.swift
+//  Roamly
+//
+//  Created by Настя on 06.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct RoamlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoamlyTests/RoamlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoamlyTests.swift
+//  RoamlyTests
+//
+//  Created by Настя on 06.04.2025.
+//
+
+import Testing
+@testable import Roamly
+
+struct RoamlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
